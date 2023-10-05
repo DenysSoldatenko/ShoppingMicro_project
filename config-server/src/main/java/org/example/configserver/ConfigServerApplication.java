c1e2434df2be6d8f2712e152342586a94076f060
DenysSoldatenko <soldatenko911@gmail.com>
2023-10-05 10:30:00 +0300

    Added config microservice

---
+++ config-server/src/main/java/org/example/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package org.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Main application class for the Config Server.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+
+}
