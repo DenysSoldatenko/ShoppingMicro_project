8bd6845c097dac5d054b069837a0924e745dfc91
DenysSoldatenko <soldatenko911@gmail.com>
2023-11-05 09:30:00 +0300

    Added security to gateway

---
+++ cloud-gateway/src/main/java/org/example/cloudgateway/configurations/OktaOauth2WebSecurity.java
@@ -0,0 +1,38 @@
+package org.example.cloudgateway.configurations;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Configuration class for setting up Okta OAuth2-based security for application.
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOauth2WebSecurity {
+
+  /**
+   * Configures the security filter chain with Okta OAuth2 settings.
+   *
+   * @param http The ServerHttpSecurity instance to configure.
+   * @return The configured SecurityWebFilterChain.
+   */
+  @Bean
+  public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+    return http
+      .authorizeExchange(
+        authorizeExchangeSpec -> authorizeExchangeSpec
+          .anyExchange().authenticated()
+      )
+      .oauth2Login(withDefaults())
+      .oauth2ResourceServer(
+        resourceServerSpec -> resourceServerSpec
+          .jwt(withDefaults())
+      )
+      .build();
+  }
+}
